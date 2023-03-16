79540853e10ad8d80b3dc4950bbb4ff70363f2bd
SimoneShaw911 <1992businessasusual@gmail.com>
2023-03-16 10:17:13 +0000

    added two scripts for running registry container

--- playbooks/aws/dockrun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo docker run \
-  -d \
-  -e ENV_DOCKER_REGISTRY_HOST=ENTER-YOUR-REGISTRY-HOST-HERE \
-  -e ENV_DOCKER_REGISTRY_PORT=ENTER-PORT-TO-YOUR-REGISTRY-HOST-HERE \
-  -p 8080:80 \
-  konradkleine/docker-registry-frontend:v2
